30a8d02042acb61e3fb2cf5c1f87fb324ec0ce7c
Zain Ali <zain8511@gmail.com>
2021-08-11 21:37:35 +0500

    4. Task completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -15,6 +15,10 @@ contract TodoList {
         string content,
         bool completed
     );
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
 
     constructor() public{
         createTask("This is a default Task. Create on deployment via constructor.");
@@ -24,5 +28,11 @@ contract TodoList {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount,_content,false);
+    }
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
--- src/app.js
+++ src/app.js
@@ -93,7 +93,7 @@ App = {
             $newTaskTemplate.find('input')
                             .prop('name', taskId)
                             .prop('checked', taskCompleted)
-                            // .on('click', App.toggleCompleted)
+                            .on('click', App.toggleCompleted)
 
             // Put the task in the correct list
             if (taskCompleted) {
@@ -111,6 +111,12 @@ App = {
         App.setLoading(true)
         const content = $('#newTask').val()
         await App.todoList.createTask(content)
+        window.location.reload()
+    },
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId)
         window.location.reload()
     },
 
